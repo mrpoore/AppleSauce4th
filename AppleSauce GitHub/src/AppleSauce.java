58ac0e4f7389522faf3be198ad10aefdc458e832
Richard Poore <mrrichpoore@gmail.com>
2015-10-20 12:27:22 -0500

    Added Greeting

--- AppleSauce GitHub/src/AppleSauce.java
+++ AppleSauce GitHub/src/AppleSauce.java
@@ -4,7 +4,7 @@ public class AppleSauce {
 	public static void main(String[] args) {
 		System.out.println("WooHoo it's Fall");
 		System.out.println("The weather is getting nice");
-
+		System.out.println("Hi");
 	}
 
 }
